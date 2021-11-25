98530d528f11f2ab0631983395974dd3c02aa0bc
arunakumari-samatham <arunakumariists@gmail.com>
2021-11-25 09:11:23 +0530

    higher order hooksapp

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="DataFetching"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>ClickCounter</h1>
+    <h1>Hovercounter</h1>
     </div>
   );
 }
